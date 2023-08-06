480dce79c1258f3dcb354526c77016ea6e7146d0
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-08-06 13:35:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+# Made by https://me.harshit.wtf
+# Import libraries 
+import streamlit as st
+from imdapy import IMD
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
 
+# Load model
+df = pd.read_csv('india_weather_data.csv')
+X = df[['temperature', 'humidity', 'pressure']]
+y = df['precipitation']
+
+model = RandomForestRegressor()
+model.fit(X, y)
+
+# API credentials
+imd = IMD(api_key)
+
+# Get forecast data 
+city = "Delhi"
+forecast = imd.get_forecast(city=city, product="three_hour")  
+
+temp = forecast['temp']
+humidity = forecast['humidity']
+pressure = forecast['pressure']
+
+# Make prediction
+data = [[temp, humidity, pressure]]
+prediction = model.predict(data)
+
+# Display  
+st.title("Weather Forecast for "+city)
+st.write("IMD Forecast:", forecast['precipitation'])
+st.write("Predicted Precipitation:", prediction[0])
